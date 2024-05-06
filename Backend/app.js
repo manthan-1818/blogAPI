17ec1386c51ac08b473c1b401639f17886e372a7
manthan-1818 <manthanvaghela1818@gmail.com>
2024-05-06 10:31:18 +0530

    .env

--- Backend/app.js
+++ Backend/app.js
@@ -23,3 +23,14 @@ app.use("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+
+
+
+
+// .env
+// PORT=5000
+// MONGODB_URI=mongodb+srv://manthanvaghela1818:vP4I8AlIZCvXgJXj@cluster0.g1mklq0.mongodb.net/myblog
+// CORS_ORIGIN=http://localhost:3000
+// JWT_SECRET_KEY = 'secret_key_';
+// JWT_REFRESH_SECRET=your_refresh_secret_key_here
--- Frontend/src/Components/Blog.js
+++ Frontend/src/Components/Blog.js
@@ -1,371 +1,7 @@
-
-// import React, { useState, useEffect } from "react";
-// import { Link } from "react-router-dom";
-// import axios from "axios";
-// import {
-//   Col,
-//   Row,
-//   Button,
-//   Modal,
-//   Form,
-// } from "react-bootstrap";
-// import { AgGridReact } from "ag-grid-react";
-// import "ag-grid-community/styles/ag-grid.css";
-// import "ag-grid-community/styles/ag-theme-alpine.css";
-// import NavComponent from "./Navbar.js";
-
-// const Blog = () => {
-//   const [showAddModal, setShowAddModal] = useState(false);
-//   const [showUpdateModal, setShowUpdateModal] = useState(false);
-//   const [showDeleteModal, setShowDeleteModal] = useState(false);
-//   const [blog, setBlog] = useState([]);
-//   const userRole = localStorage.getItem("userRole");
-
-//   const [formData, setFormData] = useState({
-//     id: "",
-//     title: "",
-//     description: "",
-//     image: null,
-//   });
-//   console.log("idddd",formData.id);
-//   const [deleteId, setDeleteId] = useState("");
-
-//   const handleCloseAddModal = () => setShowAddModal(false);
-//   const handleCloseUpdateModal = () => setShowUpdateModal(false);
-//   const handleCloseDeleteModal = () => setShowDeleteModal(false);
-  
-//   const handleChange = (e) => {
-//     const { name, value, files } = e.target;
-//     setFormData((prevFormData) => ({
-//       ...prevFormData,
-//       [name]: name === "image" ? files[0] : value,
-//     }));
-//   };
-
-//   const handleShowAddModal = () => {
-//     setShowAddModal(true);
-//     setFormData({
-//       title: "",
-//       description: "",
-//       image: null,
-//     });
-//   };
-
-//   const fetchBlogData = async () => {
-//     try {
-//       const response = await axios.get("http://localhost:5000/blog/readblog");
-//       setBlog(response.data);
-//     } catch (error) {
-//       console.error("Error fetching data:", error);
-//     }
-//   };
-
-//   const handleSubmitAdd = async (e) => {
-//     e.preventDefault();
-//     if (!formData.title || !formData.description || !formData.image) {
-//       return;
-//     }
-//     try {
-//       const formDataObject = new FormData();
-//       formDataObject.append("title", formData.title);
-//       formDataObject.append("description", formData.description);
-//       formDataObject.append("image", formData.image);
-  
-//       const response = await axios.post(
-//         "http://localhost:5000/submit/addblog",
-//         formDataObject,
-//         {
-//           headers: {
-//             "Content-Type": "multipart/form-data",
-//           },
-//         }
-//       );
-  
-//       setBlog([...blog, response.data]);
-//       setShowAddModal(false);
-//     } catch (error) {
-//       console.error("Error adding blog:", error);
-//     }
-//   };
-
-//   const handleShowUpdateModal = (params) => {
-//     const id = params.data.id;
-//     const matchedBlog = blog.find((blog) => blog.id === id);
-//     setFormData({
-//       id: matchedBlog.id,
-//       title: matchedBlog.title,
-//       description: matchedBlog.description,
-//       image: null,
-//     });
-//     setShowUpdateModal(true);
-//   };
-//   const handleSubmitUpdate = async () => {
-//     try {
-//       await axios.patch(`http://localhost:3001/blog/${formData.id}`, formData);
-//       const updatedBlogs = blog.map((item) =>
-//         item.id === formData.id ? formData : item
-//       );
-//       setBlog(updatedBlogs);
-//       setShowUpdateModal(false);
-//     } catch (error) {
-//       console.error("Error updating blog:", error);
-//     }
-//   };
-
-//   const handleDelete = async (id) => {
-//     try {
-//       await axios.delete(`http://localhost:3001/blog/${id}`);
-//       setBlog(blog.filter((blog) => blog.id !== id));
-//       setShowDeleteModal(false);
-//     } catch (error) {
-//       console.error("Error deleting blog:", error);
-//     }
-//   };
-
-//   const handleShowDeleteModal = (id) => {
-//     setDeleteId(id);
-//     setShowDeleteModal(true);
-//   };
-
-//   const editButtonRenderer = (params) => {
-//     if (userRole !== "Admin") {
-//       return (
-//         <>
-//           <Button
-//             variant="primary"
-//             size="sm"
-//             onClick={() => handleShowUpdateModal(params)}
-//           >
-//             Edit
-//           </Button>{" "}
-//           <Button
-//             variant="danger"
-//             size="sm"
-//             onClick={() => handleShowDeleteModal(params.data.id)}
-//           >
-//             Delete
-//           </Button>
-//         </>
-//       );
-//     } else {
-//       return null;
-//     }
-//   };
-
-//   const columnDefs = [
-//     { headerName: "ID", field: "id", width: 100 },
-//     {
-//       headerName: "Title",
-//       field: "title",
-//       flex: 1,
-//       cellRenderer: (params) => {
-//         const { id } = params.data;
-//         console.log("iiiiiiiiiiiiddddddddddd",id);
-//         return <Link to={`/preview/?_id=${id}`}>{params.value}</Link>;
-//       },
-//     },
-//     { headerName: "Description", field: "description", flex: 1 },
-//     {
-//       headerName: "Actions",
-//       cellRenderer: editButtonRenderer,
-//       width: 150,
-//       minWidth: 150,
-//       resizable: false,
-//     },
-//   ];
-
-//   if (userRole) {
-//     columnDefs.pop();
-//   }
-
-//   useEffect(() => {
-//     fetchBlogData();
-//   }, []);
-
-//   return (
-//     <>
-//       <NavComponent />
-//       <Row
-//         className="justify-content-center"
-//         style={{ overflow: "hidden", width: "100%" }}
-//       >
-//         <Col>
-//           <header className="text-center mb-4">
-//             <div className="d-flex justify-content-between align-items-center">
-//               <div className="flex-grow-1 text-center mt-2">
-//                 <h3>Blog App in React</h3>
-//               </div>
-//               {userRole !== "Admin" ? (
-//                 <Button
-//                   variant="primary"
-//                   onClick={handleShowAddModal}
-//                   style={{ marginRight: "18px", padding: "5px 8px" }}
-//                 >
-//                   Add Blog
-//                 </Button>
-//               ) : (
-//                 ""
-//               )}
-//             </div>
-//           </header>
-//           <div className="ag-theme-alpine" style={{ height: "500px" }}>
-//             <AgGridReact columnDefs={columnDefs} rowData={blog} />
-//           </div>
-//         </Col>
-//       </Row>
-//       <Modal
-//         show={showAddModal}
-//         onHide={handleCloseAddModal}
-//         centered
-//         className="blog-modal"
-//       >
-//         <Modal.Header closeButton>
-//           <Modal.Title>Add Blog</Modal.Title>
-//         </Modal.Header>
-//         <Modal.Body>
-//           <Form onSubmit={handleSubmitAdd}>
-//             <Form.Group controlId="formGridTitle">
-//               <Form.Label>Title *</Form.Label>
-//               <Form.Control
-//                 type="text"
-//                 placeholder="Enter title"
-//                 name="title"
-//                 value={formData.title}
-//                 onChange={handleChange}
-//               />
-//               {!formData.title && (
-//                 <Form.Text className="text-danger">Title is required</Form.Text>
-//               )}
-//             </Form.Group>
-//             <Form.Group controlId="formGridDescription">
-//               <Form.Label>Description *</Form.Label>
-//               <Form.Control
-//                 as="textarea"
-//                 rows={3}
-//                 placeholder="Enter description"
-//                 name="description"
-//                 value={formData.description}
-//                 onChange={handleChange}
-//               />
-//               {!formData.description && (
-//                 <Form.Text className="text-danger">
-//                   Description is required
-//                 </Form.Text>
-//               )}
-//             </Form.Group>
-//             <Form.Group controlId="formGridImage">
-//               <Form.Label className="modalLabel">Image</Form.Label>
-//               {formData.image && formData.image instanceof Blob && (
-//                 <img
-//                   src={URL.createObjectURL(formData.image)}
-//                   alt="Selected"
-//                   style={{ maxWidth: "100%", marginBottom: "10px" }}
-//                 />
-//               )}
-//               <Form.Control type="file" name="image" onChange={handleChange} />
-//             </Form.Group>
-//           </Form>
-//         </Modal.Body>
-//         <Modal.Footer>
-//           <Button variant="secondary" onClick={handleCloseAddModal}>
-//             Close
-//           </Button>
-//           <Button variant="primary" onClick={handleSubmitAdd}>
-//             Submit
-//           </Button>
-//         </Modal.Footer>
-//       </Modal>
-
-//       <Modal
-//         show={showUpdateModal}
-//         onHide={handleCloseUpdateModal}
-//         centered
-//         className="blog-modal"
-//       >
-//         <Modal.Header closeButton>
-//           <Modal.Title>Update Blog</Modal.Title>
-//         </Modal.Header>
-//         <Modal.Body>
-//           <Form onSubmit={handleSubmitUpdate}>
-//             <Form.Group controlId="formGridTitle">
-//               <Form.Label>Title *</Form.Label>
-//               <Form.Control
-//                 type="text"
-//                 placeholder="Enter title"
-//                 name="title"
-//                 value={formData.title}
-//                 onChange={handleChange}
-//               />
-//               {!formData.title && (
-//                 <Form.Text className="text-danger">Title is required</Form.Text>
-//               )}
-//             </Form.Group>
-//             <Form.Group controlId="formGridDescription">
-//               <Form.Label>Description *</Form.Label>
-//               <Form.Control
-//                 as="textarea"
-//                 rows={3}
-//                 placeholder="Enter description"
-//                 name="description"
-//                 value={formData.description}
-//                 onChange={handleChange}
-//               />
-//               {!formData.description && (
-//                 <Form.Text className="text-danger">
-//                   Description is required
-//                 </Form.Text>
-//               )}
-//             </Form.Group>
-//             <Form.Group controlId="formGridImage">
-//               <Form.Label className="modalLabel">Image</Form.Label>
-//               <Form.Control type="file" name="image" onChange={handleChange} />
-//             </Form.Group>
-//           </Form>
-//         </Modal.Body>
-//         <Modal.Footer>
-//           <Button variant="secondary" onClick={handleCloseUpdateModal}>
-//             Close
-//           </Button>
-//           <Button variant="primary" onClick={handleSubmitUpdate}>
-//             Submit
-//           </Button>
-//         </Modal.Footer>
-//       </Modal>
-//       <Modal
-//         show={showDeleteModal}
-//         onHide={handleCloseDeleteModal}
-//         centered
-//         className="blog-modal"
-//       >
-//         <Modal.Header closeButton>
-//           <Modal.Title>Delete Blog</Modal.Title>
-//         </Modal.Header>
-//         <Modal.Body>Are you sure you want to delete this blog?</Modal.Body>
-//         <Modal.Footer>
-//           <Button variant="secondary" onClick={handleCloseDeleteModal}>
-//             Cancel
-//           </Button>
-//           <Button variant="danger" onClick={() => handleDelete(deleteId)}>
-//             Delete
-//           </Button>
-//         </Modal.Footer>
-//       </Modal>
-//     </>
-//   );
-// };
-
-// export default Blog;
-
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import {
-  Col,
-  Row,
-  Button,
-  Modal,
-  Form,
-} from "react-bootstrap";
+import { Col, Row, Button, Modal, Form } from "react-bootstrap";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
@@ -389,7 +25,7 @@ const Blog = () => {
   const handleCloseAddModal = () => setShowAddModal(false);
   const handleCloseUpdateModal = () => setShowUpdateModal(false);
   const handleCloseDeleteModal = () => setShowDeleteModal(false);
-  
+
   const handleChange = (e) => {
     const { name, value, files } = e.target;
     setFormData((prevFormData) => ({
@@ -425,7 +61,7 @@ const Blog = () => {
       formDataObject.append("title", formData.title);
       formDataObject.append("description", formData.description);
       formDataObject.append("image", formData.image);
-  
+
       const response = await axios.post(
         "http://localhost:5000/submit/addblog",
         formDataObject,
@@ -435,7 +71,7 @@ const Blog = () => {
           },
         }
       );
-  
+
       setBlog([...blog, response.data]);
       setShowAddModal(false);
     } catch (error) {
@@ -506,13 +142,13 @@ const Blog = () => {
   };
 
   const columnDefs = [
-    { headerName: "ID", field: "_id", width: 100 }, // Use _id instead of id if that's how your data is structured
+    { headerName: "ID", field: "_id", width: 100 }, 
     {
       headerName: "Title",
       field: "title",
       flex: 1,
       cellRenderer: (params) => {
-        const { _id } = params.data; // Use _id instead of id if that's how your data is structured
+        const { _id } = params.data; 
         return <Link to={`/preview/?id=${_id}`}>{params.value}</Link>;
       },
     },
@@ -525,7 +161,6 @@ const Blog = () => {
       resizable: false,
     },
   ];
-  
 
   if (userRole) {
     columnDefs.pop();
@@ -538,7 +173,10 @@ const Blog = () => {
   return (
     <>
       <NavComponent />
-      <Row className="justify-content-center" style={{ overflow: "hidden", width: "100%" }}>
+      <Row
+        className="justify-content-center"
+        style={{ overflow: "hidden", width: "100%" }}
+      >
         <Col>
           <header className="text-center mb-4">
             <div className="d-flex justify-content-between align-items-center">
@@ -546,7 +184,11 @@ const Blog = () => {
                 <h3>Blog App in React</h3>
               </div>
               {userRole !== "Admin" ? (
-                <Button variant="primary" onClick={handleShowAddModal} style={{ marginRight: "18px", padding: "5px 8px" }}>
+                <Button
+                  variant="primary"
+                  onClick={handleShowAddModal}
+                  style={{ marginRight: "18px", padding: "5px 8px" }}
+                >
                   Add Blog
                 </Button>
               ) : (
